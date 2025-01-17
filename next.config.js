d54487ebec8ade9e4e731a66e88dc88f485f0d61
Zaymes <zaymesshrestha@gmail.com>
2025-01-17 13:43:01 +0545

    update subdomain

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
       unoptimized: true,
     },
     output: 'export',
-    basePath: '/wid-conference', // replace with your actual repo name
-    assetPrefix: '/wid-conference/',
     // Required for static HTML export
     trailingSlash: true,
     // Allow loading scripts from CDN
